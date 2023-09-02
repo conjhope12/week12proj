8b00ad534e8b5313bff0e324fce0666887c0fa87
Conrad Hope <conjhope@outlook.com>
2023-09-01 20:25:35 -0600

    last commit

--- week12proj.js
+++ week12proj.js
@@ -1,5 +1,8 @@
-// const URL_ENDPOINT = "https://64ee21d51f87218271425a8e.mockapi.io/Promineo_Tech_API/weight_room"
+// Here's my link to my ajax.
+
 const URL_ENDPOINT = 'http://localhost:3000/lightWeight'
+
+// This function allows me to go to the db and edit data from the db to the app.
 
 $.get(URL_ENDPOINT).then(data => {
     data.map(unit => {
@@ -18,6 +21,9 @@ $.get(URL_ENDPOINT).then(data => {
     })
   })
 
+// This click function allows me to make changes to the app that will effect the db.
+// >> .post allows me to add data to the existing table.
+
     $('#submitButton').on('click',function () {
   
       $.post(URL_ENDPOINT, {
@@ -25,13 +31,19 @@ $.get(URL_ENDPOINT).then(data => {
         specificLift: $('#newLift').val(),
       })
     })
-  
+
+   // The deleteUser function allows me to make changes to the app that will effect the db.
+   // >> This function allows me to remove data from the table by id.
+
   function deleteUser (id) {
 
     $.ajax(`${URL_ENDPOINT}/${id}`, {
       method: 'DELETE'
     })
   }
+
+  // The updateUser function allows me to make changes in the data already in the data base.
+  // >> PUT allows me to push changes to existing data.
 
   function updateUser() {
     let id = $('#unitID').val();
@@ -44,8 +56,10 @@ $.get(URL_ENDPOINT).then(data => {
     })
   }
   
+  // This line allows me to connect the above function to the update button.
+
   $('#submitUpdateButton').on('click',updateUser)
 
-  for (let i = "a"; i !== "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; i += "a") {
-    console.log(i)
-  }
+  // for (let i = "a"; i !== "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; i += "a") {
+  //   console.log(i)
+  // }
